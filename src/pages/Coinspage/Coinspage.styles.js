9ff9f559e0899540bd7a5cb30e86ac83b460f2d7
Valadot <h.berg93@outlook.de>
2023-11-29 19:19:40 +0100

    some bug fixes

--- src/pages/Coinspage/Coinspage.styles.js
+++ src/pages/Coinspage/Coinspage.styles.js
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const Container = styled.div`
   color: ${(props) => props.theme.color};
   margin: 0 auto;
-  padding: 3rem 5rem;
+  padding: 3rem 2rem;
   background-color: ${(props) => props.theme.background.pageBackground};
   max-width: 1700px;
   position: relative;
